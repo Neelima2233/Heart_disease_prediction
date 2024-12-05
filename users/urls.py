04d184c16a886a561fe91e981a2f3c735cd0734b
Neelima2233 <radhakarasu@gmail.com>
2024-12-05 20:20:09 +0530

    urls.py

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
@@ -7,8 +8,22 @@ urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('successfully_registered/', views.successfully_registered, name='successfully_registered'),
     path('successfully_logged_in/', views.successfully_logged_in, name='successfully_logged_in'),
-    path('patient_entry/', views.patient_entry, name='patient_entry'),
-    path('history/', views.history, name='history'),
+    
+    path('prediction_form/', views.prediction_form, name='prediction_form'),
+    path('result/', views.result, name='result'),
+
+    
+
+    path('password_reset/', auth_views.PasswordResetView.as_view(
+        template_name='password/password_reset_form.html'), name='password_reset'),
+    path('password_reset_done/', auth_views.PasswordResetDoneView.as_view(
+        template_name='password/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(
+        template_name='password/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
+        template_name='password/password_reset_complete.html'), name='password_reset_complete'),
+        
+        
 ]
 
 
